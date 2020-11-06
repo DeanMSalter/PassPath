aad3f782c4166e638ad5a1f1b9fcfaf71296841a
Dean Salter <dean.m.salter.com>
2020-11-06 18:18:24 +0000

    speed mode mvp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 let submitted = false;
+let speedMode = false;
 let pointsVisited = [];
 let panorama;
+let moveSpeedMode = 0;
+let linksCount = 0;
 function initPano(listener) {
     let headingsByPanoId = [];
+    let prevHeadingsByPanoId = [];
+
     panorama = new google.maps.StreetViewPanorama(
         document.getElementById("pano"),
         {
@@ -13,54 +18,107 @@ function initPano(listener) {
     );
 
     panorama.addListener("position_changed", function() {
-        if (submitted) {
-            return;
+        if(panorama.getLinks()) {
+            linksCount = panorama.getLinks().length
         }
-        let turnDirection = getTurnQuadrant(headingsByPanoId[panorama.pano]);
-        if (turnDirection) {
-            $("#route-cell").text($("#route-cell").text() + " " + turnDirection);
-            $("#routeRoad-cell").text($("#routeRoad-cell").text() + " " + panorama.location.shortDescription);
-        }
+        // if (submitted) {
+        //     return;
+        // }
+        // prevHeadingsByPanoId = [];
+        // for (const i in panorama.getLinks()) {
+        //     prevHeadingsByPanoId[panorama.getLinks()[i].pano] = panorama.getLinks()[i].heading;
+        // }
+        // if(speedMode ) {
+        //     let listOfLinks = [];
+        //     for (let i in panorama.getLinks()){
+        //         listOfLinks.push(panorama.getLinks()[i].heading)
+        //     }
+        //     let closest = listOfLinks.reduce(function(prev, curr) {
+        //         return (Math.abs(curr - prevHeadingsByPanoId[panorama.pano]) < Math.abs(prev - prevHeadingsByPanoId[panorama.pano]) ? curr : prev);
+        //     });
+        //     let nextLink = panorama.getLinks().filter(link => link.heading === closest)[0];
+        // }
+        // let previousLinksCount = Object.keys(headingsByPanoId).length;
+        // if (previousLinksCount > 2) {
+        //     let turnDirection = getTurnQuadrant(headingsByPanoId[panorama.pano]);
+        //     if (turnDirection) {
+        //         $("#route-cell").text($("#route-cell").text() + " " + turnDirection);
+        //         $("#routeRoad-cell").text($("#routeRoad-cell").text() + " " + panorama.location.shortDescription);
+        //     }
+        // }
+        //
+        // const positionCell = document.getElementById("position-cell");
+        // let position = panorama.getPosition();
+        // let latLng = {
+        //     lat : position.lat(),
+        //     lng : position.lng(),
+        // };
+        // pointsVisited.push(latLng);
+        // positionCell.firstChild.nodeValue = position + "";
 
-        const positionCell = document.getElementById("position-cell");
-        let position = panorama.getPosition();
-        let latLng = {
-            lat : position.lat(),
-            lng : position.lng(),
-        };
-        pointsVisited.push(latLng);
-        positionCell.firstChild.nodeValue = position + "";
+
     });
 
     panorama.addListener("links_changed", () => {
-        const linksTable = document.getElementById("links_table");
-        while (linksTable.hasChildNodes()) {
-            linksTable.removeChild(linksTable.lastChild);
-        }
-        const links = panorama.getLinks();
+        // const linksTable = document.getElementById("links_table");
+        // while (linksTable.hasChildNodes()) {
+        //     linksTable.removeChild(linksTable.lastChild);
+        // }
+        // const links = panorama.getLinks();
+        //
+        // // headingsByPanoId = [];
+        // for (const i in links) {
+        //     // headingsByPanoId[links[i].pano] = links[i].heading;
+        //     const row = document.createElement("tr");
+        //     linksTable.appendChild(row);
+        //     const labelCell = document.createElement("td");
+        //     labelCell.innerHTML = "<b>Link: " + i + "</b>";
+        //     const valueCell = document.createElement("td");
+        //     valueCell.innerHTML = links[i].description;
+        //     linksTable.appendChild(labelCell);
+        //     linksTable.appendChild(valueCell);
+        // }
 
-        headingsByPanoId = [];
-        for (const i in links) {
-            if (links.length > 2) {
-                headingsByPanoId[links[i].pano] = links[i].heading;
+
+        let nextLink;
+        if(speedMode ) {
+            let listOfLinks = [];
+            for (let i in panorama.getLinks()){
+                listOfLinks.push(panorama.getLinks()[i].heading)
             }
-            const row = document.createElement("tr");
-            linksTable.appendChild(row);
-            const labelCell = document.createElement("td");
-            labelCell.innerHTML = "<b>Link: " + i + "</b>";
-            const valueCell = document.createElement("td");
-            valueCell.innerHTML = links[i].description;
-            linksTable.appendChild(labelCell);
-            linksTable.appendChild(valueCell);
-
+            let closest = listOfLinks.reduce(function(prev, curr) {
+                return (Math.abs(curr - prevHeadingsByPanoId[panorama.pano]) < Math.abs(prev - prevHeadingsByPanoId[panorama.pano]) ? curr : prev);
+            });
+            nextLink = panorama.getLinks().filter(link => link.heading === closest)[0];
+        }
+        prevHeadingsByPanoId = [];
+        for (const i in panorama.getLinks()) {
+            prevHeadingsByPanoId[panorama.getLinks()[i].pano] = panorama.getLinks()[i].heading;
+        }
+        console.log("-------------");
+        if(speedMode && panorama.getLinks().length < 3  && moveSpeedMode <= 20) {
+            setTimeout(function(){
+                panorama.setPano(nextLink.pano);
+            }, 1000);
+            moveSpeedMode += 1;
+        } else {
+            console.log("STOPPED")
+            console.log(linksCount)
+            console.log(panorama.getLinks())
+            console.log(moveSpeedMode)
         }
     });
 }
 
 
 $(document).on('keypress',function(e) {
+    console.log(e)
     if (e.which === 112 ) {
         submitted = true;
+    }
+    if(e.which === 105) {
+        moveSpeedMode = 0;
+        speedMode = !speedMode;
     }
 });
 
