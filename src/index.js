e55f82baf10a8b930531c261f5ab357ce985319e
Dean Salter <dean.m.salter.com>
2020-11-14 17:37:33 +0000

    fixed speed mode and made code less messy #1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ let pointsVisited = [];
 let panorama;
 let moveSpeedMode = 0;
 let linksCount = 0;
+let leftJunction = true;
 function initPano(listener) {
     let headingsByPanoId = [];
     let prevHeadingsByPanoId = [];
@@ -14,6 +15,8 @@ function initPano(listener) {
             //lat and lng outside union building
             position: { lat: 50.794502, lng: -1.097004 },
             visible: true,
+            clickToGo: false,
+            scrollwheel: false,
         }
     );
 
@@ -21,91 +24,76 @@ function initPano(listener) {
         if(panorama.getLinks()) {
             linksCount = panorama.getLinks().length
         }
-        // if (submitted) {
-        //     return;
-        // }
-        // prevHeadingsByPanoId = [];
-        // for (const i in panorama.getLinks()) {
-        //     prevHeadingsByPanoId[panorama.getLinks()[i].pano] = panorama.getLinks()[i].heading;
-        // }
-        // if(speedMode ) {
-        //     let listOfLinks = [];
-        //     for (let i in panorama.getLinks()){
-        //         listOfLinks.push(panorama.getLinks()[i].heading)
-        //     }
-        //     let closest = listOfLinks.reduce(function(prev, curr) {
-        //         return (Math.abs(curr - prevHeadingsByPanoId[panorama.pano]) < Math.abs(prev - prevHeadingsByPanoId[panorama.pano]) ? curr : prev);
-        //     });
-        //     let nextLink = panorama.getLinks().filter(link => link.heading === closest)[0];
-        // }
-        // let previousLinksCount = Object.keys(headingsByPanoId).length;
-        // if (previousLinksCount > 2) {
-        //     let turnDirection = getTurnQuadrant(headingsByPanoId[panorama.pano]);
-        //     if (turnDirection) {
-        //         $("#route-cell").text($("#route-cell").text() + " " + turnDirection);
-        //         $("#routeRoad-cell").text($("#routeRoad-cell").text() + " " + panorama.location.shortDescription);
-        //     }
-        // }
-        //
-        // const positionCell = document.getElementById("position-cell");
-        // let position = panorama.getPosition();
-        // let latLng = {
-        //     lat : position.lat(),
-        //     lng : position.lng(),
-        // };
-        // pointsVisited.push(latLng);
-        // positionCell.firstChild.nodeValue = position + "";
+        if (submitted) {
+            return;
+        }
+        let previousLinksCount = Object.keys(headingsByPanoId).length;
+        if (previousLinksCount > 2) {
+            let turnDirection = getTurnQuadrant(headingsByPanoId[panorama.pano]);
+            if (turnDirection) {
+                $("#route-cell").text($("#route-cell").text() + " " + turnDirection);
+                $("#routeRoad-cell").text($("#routeRoad-cell").text() + " " + panorama.location.shortDescription);
+            }
+        }
 
-
+        const positionCell = document.getElementById("position-cell");
+        let position = panorama.getPosition();
+        let latLng = {
+            lat : position.lat(),
+            lng : position.lng(),
+        };
+        pointsVisited.push(latLng);
+        positionCell.firstChild.nodeValue = position + "";
     });
 
     panorama.addListener("links_changed", () => {
-        // const linksTable = document.getElementById("links_table");
-        // while (linksTable.hasChildNodes()) {
-        //     linksTable.removeChild(linksTable.lastChild);
-        // }
-        // const links = panorama.getLinks();
-        //
-        // // headingsByPanoId = [];
-        // for (const i in links) {
-        //     // headingsByPanoId[links[i].pano] = links[i].heading;
-        //     const row = document.createElement("tr");
-        //     linksTable.appendChild(row);
-        //     const labelCell = document.createElement("td");
-        //     labelCell.innerHTML = "<b>Link: " + i + "</b>";
-        //     const valueCell = document.createElement("td");
-        //     valueCell.innerHTML = links[i].description;
-        //     linksTable.appendChild(labelCell);
-        //     linksTable.appendChild(valueCell);
-        // }
+        const linksTable = document.getElementById("links_table");
+        while (linksTable.hasChildNodes()) {
+            linksTable.removeChild(linksTable.lastChild);
+        }
+        const links = panorama.getLinks();
+
+        headingsByPanoId = [];
+        for (const i in links) {
+            headingsByPanoId[links[i].pano] = links[i].heading;
+            const row = document.createElement("tr");
+            linksTable.appendChild(row);
+            const labelCell = document.createElement("td");
+            labelCell.innerHTML = "<b>Link: " + i + "</b>";
+            const valueCell = document.createElement("td");
+            valueCell.innerHTML = links[i].description;
+            linksTable.appendChild(labelCell);
+            linksTable.appendChild(valueCell);
+        }
 
 
         let nextLink;
         if(speedMode ) {
-            let listOfLinks = [];
-            for (let i in panorama.getLinks()){
-                listOfLinks.push(panorama.getLinks()[i].heading)
+            //angles will be calculated from 0 - 180 in closeness so 181 will always be overridden.
+            let distanceFromTargetAngle = 181;
+            for (let link in panorama.getLinks()) {
+                let  phi = Math.abs(panorama.getLinks()[link].heading - prevHeadingsByPanoId[panorama.pano]) % 360;       // This is either the distance or 360 - distance
+                let  distance = phi > 180 ? 360 - phi : phi;
+                if (distance < distanceFromTargetAngle) {
+                    distanceFromTargetAngle = distance;
+                    nextLink = panorama.getLinks()[link];
+                }
             }
-            let closest = listOfLinks.reduce(function(prev, curr) {
-                return (Math.abs(curr - prevHeadingsByPanoId[panorama.pano]) < Math.abs(prev - prevHeadingsByPanoId[panorama.pano]) ? curr : prev);
-            });
-            nextLink = panorama.getLinks().filter(link => link.heading === closest)[0];
         }
+
         prevHeadingsByPanoId = [];
         for (const i in panorama.getLinks()) {
             prevHeadingsByPanoId[panorama.getLinks()[i].pano] = panorama.getLinks()[i].heading;
         }
-        console.log("-------------");
-        if(speedMode && panorama.getLinks().length < 3  && moveSpeedMode <= 20) {
+        if(speedMode && panorama.getLinks().length < 3  && moveSpeedMode <= 20 && leftJunction) {
             setTimeout(function(){
                 panorama.setPano(nextLink.pano);
             }, 1000);
             moveSpeedMode += 1;
-        } else {
-            console.log("STOPPED")
-            console.log(linksCount)
-            console.log(panorama.getLinks())
-            console.log(moveSpeedMode)
+        } else if (panorama.getLinks().length > 2){
+            leftJunction = false;
+        } else if (panorama.getLinks().length < 3) {
+            leftJunction = true;
         }
     });
 }
